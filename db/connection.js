5318bddef29dbf085520f596ed51177177dc41bc
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-08-31 15:40:54 -0400

    initialize app and show user prompt

--- db/connection.js
+++ db/connection.js
@@ -8,9 +8,9 @@ const db = mysql.createConnection(
         user: 'root',
         // Your MySQL password
         password: '',
-        database: 'election'
+        database: 'company'
     },
-    console.log('Connected to the election database.')
+    console.log('Connected to the company database.')
 );
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const inquirer = require('inquirer');
+const db = require('./db/connection');
+const cTable = require('console.table');
+
+// Options for user prompt
+const selectOption = [
+    {
+        type: 'list',
+        name: 'userSelected',
+        message: 'Welcome to the Employee Manager! What would you like to do?',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add department', 'Add role', 'Add employee', 'Update employee role'],
+        default: 'View all departments'
+    },
+];
+
+// Function to initialize app
+function init(options) {
+    return inquirer.prompt(options)
+};
+
+// Function call to initialize app
+init(selectOption)
